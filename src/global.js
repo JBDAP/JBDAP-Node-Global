a4732b027080dd34ec30f074c799e534d8466116
JBDAP <jbdapteam@outlook.com>
2019-06-16 02:24:33 +0800

    +requireR, +sleep

--- src/global.js
+++ src/global.js
@@ -7,6 +7,16 @@ if (global.$JBDAP_SYS === undefined) global.$JBDAP_SYS = {}
 // JS 是 JBDAP_SYS 的缩写
 let JS = global.$JBDAP_SYS
 
+/**
+ * 自定义一个模块引用的全局方法
+ * 它接受一个从根目录写起的模块路径作为参数
+ * 用于取代node自带的require
+ * path = '/dir/module.js'
+ */
+JS.requireR = function(path) {
+    return require(process.cwd()+path)
+}
+
 // 换行符
 import { EOL } from 'os'
 JS.EOL = EOL
@@ -36,6 +46,7 @@ JS.newErrorI18N = function(name,cause,info,dict,lang){
         }
     )
 }
+
 // 将 NiceError 对象抛出
 JS.throwErrorI18N = function(name,cause,info,dict,lang){
     throw JS.newErrorI18N(name,cause,info,dict,lang)
@@ -59,6 +70,14 @@ JS.exec = (promise) => {
             }
         })
 }
+
+/**
+ * 全局sleep函数，将应用程序阻塞一段时间
+ * 可以用 await 调用
+ */
+JS.sleep = (timeoutMS) => new Promise((resolve) => {
+    setTimeout(resolve, timeoutMS)
+})
 
 // 挂载 lodash
 import _ from 'lodash'
